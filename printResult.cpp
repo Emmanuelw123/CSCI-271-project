265cc803c7c8128aaad7f27b04fb1fb130a097dc
Emmanuelw123 <158106049+Emmanuelw123@users.noreply.github.com>
2026-05-07 22:48:26 -0400

    Clean up comments in printResult.cpp
    Removed duplicate comment lines in printResults function.
    
    Signed-off-by: Emmanuelw123 <158106049+Emmanuelw123@users.noreply.github.com>

--- printResult.cpp
+++ printResult.cpp
@@ -2,8 +2,6 @@
 using namespace std;
 // FUNCTION 2:
 // Displays the final placement of a player.
-// If the player did not finish, it prints a blank line.// FUNCTION 2:
-// Displays the final placement of a player.
 // If the player did not finish, it prints a blank line.
 void printResults(int place, int playerNum) {
     if (place > 0)
