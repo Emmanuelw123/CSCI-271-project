189810a66ead37b2aa58fd32806f7b887c8c74d3
Emmanuelw123 <158106049+Emmanuelw123@users.noreply.github.com>
2026-05-07 22:49:32 -0400

    Added comments
    Add function to check if the game is over based on finishing spots.
    
    Signed-off-by: Emmanuelw123 <158106049+Emmanuelw123@users.noreply.github.com>

--- isGameOver.cpp
+++ isGameOver.cpp
@@ -1,3 +1,6 @@
+// FUNCTION 3:
+// Checks if all finishing spots have been filled.
+// Returns true if the game should end.
 bool isGameOver(int place, int maxFinishers) {
     return (place > maxFinishers);
 }
