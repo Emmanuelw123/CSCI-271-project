dc10828872d9241613bb92e8b45bea9ada348bb5
Emmanuelw123 <158106049+Emmanuelw123@users.noreply.github.com>
2026-05-07 22:47:37 -0400

    Add comments for printResults function
    Added comments to describe the printResults function.
    
    Signed-off-by: Emmanuelw123 <158106049+Emmanuelw123@users.noreply.github.com>

--- printResult.cpp
+++ printResult.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 using namespace std;
+// FUNCTION 2:
+// Displays the final placement of a player.
+// If the player did not finish, it prints a blank line.// FUNCTION 2:
+// Displays the final placement of a player.
+// If the player did not finish, it prints a blank line.
 void printResults(int place, int playerNum) {
     if (place > 0)
         cout << "Player " << playerNum << " finished in place " << place << endl;
